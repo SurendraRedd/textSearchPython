a8e0b770c67f765a35acbdf0da4a79049caf7766
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2022-05-03 22:24:28 +0530

    initial development
    initial development

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+This application contains the code to search the text.
+"""
+
+__author__ = 'onesolution'
+__version__ = '1.0'
+__maintainer__ = 'onesolution'
+__email__ = 'onesolution@gmail.com'
+__status__ = 'Prototype'
+
+print('# ' + '=' * 78)
+print('Author: ' + __author__)
+print('Version: ' + __version__)
+print('Maintainer: ' + __maintainer__)
+print('Email: ' + __email__)
+print('Status: ' + __status__)
+print('# ' + '=' * 78)
